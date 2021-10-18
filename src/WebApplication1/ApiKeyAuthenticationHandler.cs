8f7014a52825cbc6bd7f262389030bcc7bc72901
심승용 <seungyongshim@nexon.co.kr>
2021-10-18 18:28:22 +0900

    코드 추가

--- src/WebApplication1/ApiKeyAuthenticationHandler.cs
+++ src/WebApplication1/ApiKeyAuthenticationHandler.cs
@@ -1,6 +1,9 @@
 namespace WebApplication1
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Claims;
     using System.Text.Encodings.Web;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Authentication;
@@ -9,13 +12,17 @@ namespace WebApplication1
 
     public class ApiKeyAuthenticationHandler : AuthenticationHandler<ApiKeyAuthenticationOptions>
     {
-        public ApiKeyAuthenticationHandler(IOptionsMonitor<ApiKeyAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        public ApiKeyAuthenticationHandler(IOptionsMonitor<ApiKeyAuthenticationOptions> options,
+                                           ILoggerFactory logger,
+                                           UrlEncoder encoder,
+                                           ISystemClock clock,
+                                           IGetApiKeyQuery getApiKeyQuery) : base(options, logger, encoder, clock)
         {
-
+            GetApiKeyQuery = getApiKeyQuery ?? throw new ArgumentNullException(nameof(getApiKeyQuery));
         }
 
         public string ApiKeyHeaderName { get; } = "X-Api-Key";
-
+        public IGetApiKeyQuery GetApiKeyQuery { get; }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
@@ -25,6 +32,33 @@ namespace WebApplication1
             }
 
             var providedApiKey = apiKeyHeaderValues.FirstOrDefault();
+
+            if (apiKeyHeaderValues.Count == 0 || string.IsNullOrWhiteSpace(providedApiKey))
+            {
+                return AuthenticateResult.NoResult();
+            }
+
+            var existingApiKey= await GetApiKeyQuery.Excute(providedApiKey);
+
+            if (existingApiKey != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, existingApiKey.Owner)
+                };
+
+                claims.AddRange(existingApiKey.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+                var identities = new List<ClaimsIdentity>
+                {
+                    new ClaimsIdentity(claims, Options.AuthenticationType)
+                };
+
+                var principal = new ClaimsPrincipal(identities);
+                var ticket = new AuthenticationTicket(principal, Options.Scheme);
+
+                return AuthenticateResult.Success(ticket);
+            }
 
             return AuthenticateResult.NoResult();
         }
