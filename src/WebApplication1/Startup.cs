12cf84eb1135a4a94b8eac00607221ff59cd3930
심승용 <seungyongshim@nexon.co.kr>
2021-10-22 16:15:34 +0900

    의존성 주입 추가

--- src/WebApplication1/Startup.cs
+++ src/WebApplication1/Startup.cs
@@ -1,17 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication1
 {
+    
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -24,6 +21,8 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IGetApiKeyQuery, InMemoryGetApiKeyQuery>();
+
             services.AddAuthentication(option =>
             {
                 option.DefaultAuthenticateScheme = ApiKeyAuthenticationOptions.DefaultScheme;
